9548c23bfdd938daab521f1a861185f1bc74ba23
RamKumar337 <jramkumar805@gmail.com>
2022-06-06 11:35:30 +0530

    changes

---
+++ Student.java
@@ -0,0 +1,12 @@
+public Student
+{
+	public static void main(String args[])
+	{
+		
+		string name="ramkumar";
+		int age=25;
+		school="goverment college";
+		course= "BSC";
+	}
+	
+}
---
+++ Teacher.java
@@ -0,0 +1,12 @@
+public Student
+{
+	public static void main(String args[])
+	{
+		
+		string name="raja";
+		int age=35;
+		college="goverment college";
+		department="cse";
+	}
+	
+}
